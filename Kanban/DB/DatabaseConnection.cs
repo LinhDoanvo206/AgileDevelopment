56ce66b2dc82eb566add941beb8fb7aa1d685448
Nikki Kononov <nikitakononov93@gmail.com>
2014-10-31 19:34:03 +0200

    Database API Update
    executeQueryCommand
    executeNonQueryCommand
    getReader
    
    Explanation of each method is inside the file under comments.

--- Kanban/DB/DatabaseConnection.cs
+++ Kanban/DB/DatabaseConnection.cs
@@ -9,28 +9,51 @@ namespace Kanban
 {
     public class DatabaseConnection
     {
-        String projectPath = @"|DataDirectory|\ProjectDatabase.mdb";                                                //Relative path for the database to avoid DB not found
-        String connectionString;
-        OleDbConnection DBConnection = new OleDbConnection();
-        OleDbCommand DBCommand = new OleDbCommand();
-        OleDbDataReader DBReader;
-        String SQLResults;
+        private String projectPath = @"|DataDirectory|\ProjectDatabase.mdb";                                         // Relative path for the database to avoid DB not found
+        private String connectionString;
+        private OleDbConnection DBConnection = new OleDbConnection();
+        private OleDbCommand DBCommand = new OleDbCommand();
+        private OleDbDataReader DBReader;
+        private String SQLResults;
 
         public void OpenConnection()
         {
+            if (!DBConnection.State.Equals(ConnectionState.Open))
+            {
+                connectionString = "Provider = Microsoft.Jet.OLEDB.4.0;" + "Data Source = " + projectPath;
+                DBConnection.ConnectionString = connectionString;
 
-            connectionString = "Provider = Microsoft.Jet.OLEDB.4.0;" + "Data Source = " + projectPath;
-            DBConnection.ConnectionString = connectionString;
-
-            DBConnection.Open();
+                DBConnection.Open();
+            }
         }
 
         public void CloseConnection()
         {
-            DBConnection.Close();
+
+            if (!DBConnection.State.Equals(ConnectionState.Closed))
+                DBConnection.Close();
         }
 
-        public Boolean executeCommand(String SQLCommand)                                                            // Executes the command submited by the method. Use it when you want to submit the command such as INSERT which also will return value TRUE if command submitted sucessfully. 
+        public Boolean executeQueryCommand(String SQLCommand)                                                       // Executes the query command that has retrunable data submited by the method. Use it when you want to submit the command such as INSERT which also will return value TRUE if command submitted sucessfully. 
+        {
+            DBCommand.Connection = DBConnection;
+            DBCommand.CommandText = SQLCommand;
+            DBCommand.CommandType = CommandType.Text;
+
+            try
+            {
+                DBReader = DBCommand.ExecuteReader();
+                return true;
+            }
+
+            catch (Exception error)
+            {
+                System.Diagnostics.Debug.WriteLine(error.ToString());                                               // Prints the error debug info in the console. Only works during DEBUG option. Use DEBUG -> Start Debugging if you want to see the error message.
+                return false;
+            }
+        }
+
+        public Boolean executeNonQueryCommand(String SQLCommand)                                                    // Executes the non-query command that has non-returnable data (such as INSERT) submited by the method. Use it when you want to submit the command such as INSERT which also will return value TRUE if command submitted sucessfully. 
         {
             DBCommand.Connection = DBConnection;
             DBCommand.CommandText = SQLCommand;
@@ -46,12 +69,12 @@ namespace Kanban
             {
                 System.Diagnostics.Debug.WriteLine(error.ToString());                                               // Prints the error debug info in the console. Only works during DEBUG option. Use DEBUG -> Start Debugging if you want to see the error message.
                 return false;
-            }      
+            }
         }
 
-        public String readDataCommand()                                                                             // Returns the information submited by the command SELECT as STRING. 
+        public OleDbDataReader getReader()                                                                    // Returns the reader to read from the other method. Use Reader methods to loop through. 
         {
-            return SQLResults;
+            return DBReader;
         }
     }
 }
