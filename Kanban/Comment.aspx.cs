7f2629a70ce7343563d7e7d3b56496fdab2bca3e
Nikki Kononov <nikitakononov93@gmail.com>
2014-11-15 17:38:37 +0200

    Method for commeting
    Implmeneted a method to add a comment to a specific task ID. This is an
    inital commit with only method implemented. Nothing else has been done
    due to missing MainActivity Table.
    
    To-Do:
    
    Display Window as a popup
    Link it to the button on the main table
    Polish the interface
    Add look at all comments feature

---
+++ Kanban/Comment.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Kanban
+{
+    public partial class Comment : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            int id = 1;
+            submitComment(id, commentToAddTextbox.Text.ToString());
+        }
+
+        protected void submitComment(int taskID, String message)
+        {
+            DatabaseConnection DBConnetion = new DatabaseConnection();
+            DBConnetion.OpenConnection();
+            if (DBConnetion.executeNonQueryCommand("INSERT INTO Comment (Task_ID, Comment)" + "VALUES ('" + taskID + "', '" + message + "')") == false)
+            {
+                commentToAddTextbox.Text = "Oops, something went wrong!";
+            }
+            DBConnetion.CloseConnection();
+        }
+    }
+}
